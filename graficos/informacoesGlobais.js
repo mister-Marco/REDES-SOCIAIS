0b9859c0cf5a413b1ce734db5b611024b2b31c2b
Mister M <112970459+mister-Marco@users.noreply.github.com>
2024-11-07 13:20:21 +0000

    07/11/24

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,11 +3,15 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
     async function visualizarInformacoesGlobais() {
         const res = await fetch(url)
         const dados = await res.json()
-        console.log(dados);
+        const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+        const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+        const horas = parseInt(dados.tempo_medio)
+        const minutos = Math.round((dados.tempo_medio - horas) * 100)
+
         const paragrafo = document.createElement('p')
-        paragrafo.classList.add('graficos-container_texto')
-        paragrafo.innerHTML = `Você sabia que o undo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} pessoas estão conectadas em alguma rede social,  e passa em média ${dados.tempo_medio} de horas conectadas.`
-        console.log(paragrafo)
+        paragrafo.classList.add('graficos-container__texto')
+        paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões </span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões </span> de pessoas estão conectadas em alguma rede social,  e passam em média <span>${horas} horas </span> e <span>${minutos} minutos </span> conectadas.`
+       
 
         const container = document.getElementById('graficos-container')
         container.appendChild(paragrafo)
